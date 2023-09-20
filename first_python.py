34f0440715e2c2d639658071580629ea2b185b98
darrencheng97 <chengxuewen97@gmail.com>
2023-09-20 22:33:39 +0800

    first commit

---
+++ first_python.py
@@ -0,0 +1,29 @@
+print("Hello World")
+
+# Basic data type & variable
+
+# String
+"Hello World"
+
+#numeric
+70
+-87
+160.5
+160.5555555
+
+#Boolean
+True
+False
+
+name = "xiaohei"
+age = 23
+is_male = True
+
+# The variable name can only the combination of english alphabet or numberic or underscore (_)
+# In the beginning of the variable naming cannot be numeric
+
+print ("There is a guy call "+ name)
+print (name+" age is "+str(age))
+print (name+" hate he is "+str(age))
+
+
